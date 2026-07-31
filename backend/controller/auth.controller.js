ca3933942183a809d59e01d1cf97e800ab772d24
Ritik singh <ritikkushwaha415@gmail.com>
2026-07-31 17:42:17 +0530

    Update cookie settings for token in auth controller

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -36,8 +36,8 @@ export const signup = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "strict",
-      secure: false
+      sameSite: "None",
+      secure: true
     })
 
     return res.status(201).json(user);
@@ -76,8 +76,8 @@ export const signin = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "strict",
-      secure: false
+      sameSite: "None",
+      secure: true
     })
 
     return res.status(201).json(user);
